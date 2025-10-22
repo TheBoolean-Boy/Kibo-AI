974ee725a936ae7ad6f2c4754324e27a35ddbfe0
TheBoolean-Boy <saswatrath02@gmail.com>
2025-10-22 17:55:35 +0530

    Convex shared package added

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,14 +1,19 @@
-import { add } from "@workspace/math/add"
-import { Button } from "@workspace/ui/components/button"
+'use client'
+
+
+import { useQuery } from "convex/react"
+import { api } from "@workspace/backend/_generated/api";
 
 export default function Page() {
+
+  const user = useQuery(api.users.getMany);
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello, Apps/widget!</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(2, 4)}</p>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p className=" text-4xl text-muted-foreground">App/widget </p>
+      <div className=" max-w-sm w-full mx-auto">
+        {JSON.stringify(user, null, 2)}
       </div>
+      
     </div>
   )
 }
